bec23d60ae1acb4322028d70d983afb48ae2dbe7
abulubabah <rezamalikdrive@gmail.com>
2020-02-21 11:30:49 +0700

    trying controllers and views

---
+++ application/views/test.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Codeigniter View Example</title>
+	</head>
+	<body>
+		<div style="color: red; font-weight: bolder; font-size: 40px;">Codeigniter View Example</div>
+	</body>
+</html>
