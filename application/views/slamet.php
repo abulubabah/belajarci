84c2625f3a805eeb2f0ab3552fcd6e8060f911dc
abulubabah <rezamalikdrive@gmail.com>
2020-03-03 08:51:31 +0700

    try inserting CSS into CI

---
+++ application/views/slamet.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>slamet is in the house</title>
+</head>
+<body>
+	<!-- <div style="color:red; font-weight: bolder; font-size: 300px;">SLAMET YAH!</div> -->
+</body>
+</html>
